bda37674d30269152f7538601a1f0edb0b35131b
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-09-14 10:55:36 +0200

    added action graphs

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -13,7 +13,7 @@ class OrderController extends Controller
     public function index(Request $request)
     {
         // Recuperiamo gli ordini
-        $orders = Order::where('restaurant_id', Auth::id())->orderBy('created_at')->get();
+        $orders = Order::where('restaurant_id', Auth::id())->orderByDesc('created_at')->get();
 
         // Recuperiamo le informazioni del ristorante
         $restaurant = Restaurant::whereId(Auth::id())->first();
@@ -47,7 +47,49 @@ class OrderController extends Controller
             abort(404);
         };
 
+        $total_price = 0;
+        foreach ($dishes as $dish) {
+            $total_price += $dish->pivot->total_price;
+        }
 
-        return view('admin.orders.show', compact('order', 'restaurant_name', 'dishes'));
+        return view('admin.orders.show', compact('order', 'restaurant_name', 'dishes', 'total_price'));
+    }
+
+    public function graphs(Request $request)
+    {
+        // Recuperiamo gli ordini
+        $orders = Order::where('restaurant_id', Auth::id())->orderBy('created_at')->get();
+
+        // Recuperiamo le informazioni del ristorante
+        $restaurant = Restaurant::whereId(Auth::id())->first();
+        $restaurant_name = $restaurant->restaurant_name;
+
+        // Recupero i piatti
+        $dishes = $restaurant->dishes()->get();
+
+        // Recupero i piatti del singolo ordine
+        $total_dishes = [];
+        foreach ($orders as $order) {
+
+            $order_dishes = $order->dishes()->get();
+
+
+            foreach ($order_dishes as $dish) {
+                $total_dishes[] = $dish['name'];
+            }
+        }
+
+        $dish_counts = [];
+        foreach ($total_dishes as $dish) {
+            if (isset($dish_counts[$dish])) {
+                // Se il titolo esiste già nell'array di conteggio, incrementa il valore
+                $dish_counts[$dish]++;
+            } else {
+                // Altrimenti, inizializza il conteggio a 1
+                $dish_counts[$dish] = 1;
+            }
+        }
+
+        return view('admin.orders.graphs', compact('orders', 'restaurant_name', 'dishes', 'dish_counts'));
     }
 }
