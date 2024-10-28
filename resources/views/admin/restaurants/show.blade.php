9f18b91fe5354aa27eaa2a481757c720639a4a93
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-10-28 16:49:39 +0100

    fixed responsive of the restaurant show

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -6,18 +6,18 @@
     <section id="restaurant-show">
         <div class="data-card spacing p-4">
             <div class="card-header mt-2">
-                <h1 class="text-center mb-5">{{$restaurant->restaurant_name}}</h1>
+                <h1 class="text-center mb-4">{{$restaurant->restaurant_name}}</h1>
             </div>
             <div class="card-body">
-                <div class="row justify-content-around mb-4">
+                <div class="row justify-content-around mb-md-4 gap-4 gap-md-0">
 
                     {{-- Colonna dell'immagine --}}
-                    <div class="col-3 d-flex align-items-center">
+                    <div class="col-10 col-md-5 col-lg-4 d-flex align-items-center">
                         <img src="{{asset('storage/'. $restaurant->image)}}" alt="{{$restaurant->restaurant_name}}" class="img-fluid rounded">
                     </div>
 
                     {{-- Colonna delle informazioni --}}
-                    <div class="col-3 d-flex align-items-center justify-content-center">
+                    <div class="col-10 col-md-4 col-lg-4 d-flex align-items-center justify-content-md-center">
                         <div>
                             <p><i class="fa-solid fa-map-pin me-2"></i>{{$restaurant->address}}</p>
                             <p><i class="fa-solid fa-location-dot me-2"></i>{{$restaurant->city}}</p>
@@ -31,7 +31,7 @@
                     </div>
 
                     {{-- Colonna del menù --}}
-                    <div class="col-3 text-center">
+                    <div class="col-md-3 col-lg-4 text-center d-none d-md-block">
                         <a href="{{route('admin.dishes.index')}}" class="effect">
                             <img class="img-fluid w-50" src="{{asset('img/menu.png')}}" alt="menu">
                             <h3 class="mt-4">Vedi il tuo menu</h3>
@@ -41,9 +41,13 @@
             </div>
 
             {{-- Bottoni --}}
-            <div class="card-footer mt-4">
-                <div class="d-flex justify-content-between align-items-center">
-                    <a href="{{route('dashboard')}}" class="btn-outline-index text-white fw-semibold gray ms-1 px-3 py-2 rounded-pill"><i class="fa-solid fa-left-long me-2"></i>Torna indietro</a>
+            <div class="card-footer">
+                <div class="row justify-content-around">
+
+                    <div class="d-flex justify-content-between align-items-center col-10 col-md-12">
+                        <a href="{{route('dashboard')}}" class="btn-outline-index text-white fw-semibold gray px-3 py-2 rounded-pill"><i class="fa-solid fa-left-long me-2"></i>Torna indietro</a>
+                    </div>
+                    
                 </div>
             </div>
         </div>
